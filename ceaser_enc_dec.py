80502d31c9840b265dd48a8db52a289a4c812d71
Battlearmour2000 <61649287+Battlearmour2000@users.noreply.github.com>
2022-09-15 21:11:22 +0300

    Add files via upload

---
+++ ceaser_enc_dec.py
@@ -0,0 +1,43 @@
+from email import message
+
+
+choice=int(input(' Choose 1 to encrypt a message: \n Choose 2 to decrypt a message:'))
+
+if choice == 1:
+
+    plaintext = input('Enter plaintext: ')
+    shift = int(input("Enter shift amount: "))
+
+    def ceasar_cipher_enc(plaintext,shift):
+        text=''
+        for char in plaintext:
+            if text == " ":
+                text=text+char
+            elif char.isupper():
+                text = text +chr((ord(char)+ shift-65)%26+65)
+            else:
+                text = text +chr((ord(char)+ shift-97)%26+97)
+        return text
+
+
+    print("Encrypted message is: " ,ceasar_cipher_enc(plaintext, shift))
+elif choice ==2:
+
+    gibber = input('Enter message: ')
+    shift = int(input("Enter shift amount: "))
+
+    def ceasar_cipher_dec(gibber,shift):
+        text=''
+        for char in gibber:
+            if text == " ":
+                text=text+char
+            elif char.isupper():
+                text = text +chr((ord(char)- shift-65)%26+65)
+            else:
+                text = text +chr((ord(char)- shift-97)%26+97)
+        return text
+
+
+    print("Encrypted message is: " ,ceasar_cipher_dec(gibber, shift))
+else:
+    print('make a right choice dumbass')
